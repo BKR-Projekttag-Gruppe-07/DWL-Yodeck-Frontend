150ff3c36dc4bf28831bfa957bf10da227a1c9c1
mroev <roevemeier.malte@gmail.com>
2024-01-23 11:42:39 +0100

    corrected variable names

--- StaticHtml/frontend.js
+++ StaticHtml/frontend.js
@@ -7,9 +7,9 @@ function connectWebSocket() {
         const receivedMessage = event.data;
         const parts = receivedMessage.split(";");
         
-        document.getElementById("test").textContent = parts[0];
-        document.getElementById("year").textContent = parts[1] + " " + parts[2];
-        document.getElementById("name").textContent = parts[3];
+        document.getElementById("section1").textContent = parts[0];
+        document.getElementById("section2").textContent = parts[1] + " " + parts[2];
+        document.getElementById("section3").textContent = parts[3];
     };
 
     socket.onerror = function (event) {
