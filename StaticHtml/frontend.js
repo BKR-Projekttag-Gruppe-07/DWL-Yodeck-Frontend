3787838889622c3021fdf5481598e93895f3ba96
Max <1059134@bkrnet.de>
2024-01-23 11:38:40 +0100

    Websocket integration sowie anzeige der Gesendeten daten

--- StaticHtml/frontend.js
+++ StaticHtml/frontend.js
@@ -3,28 +3,17 @@ let socket;
 function connectWebSocket() {
     socket = new WebSocket("ws://localhost:8887");
 
-    socket.onopen = function (e) {
-        console.log("Verbindung hergestellt!");
-        // Dokumentelement mit der ID "name" aktualisieren, wenn die Verbindung hergestellt ist
-        document.getElementById("name").textContent = "WebSocket Verbunden";
-    };
-
     socket.onmessage = function (event) {
         const receivedMessage = event.data;
-        // Dokumentelement mit der ID "name" mit dem empfangenen String aktualisieren
-        document.getElementById("name").textContent = receivedMessage;
-    };
-
-    socket.onclose = function (event) {
-        console.log("Verbindung geschlossen!", event);
-        // Dokumentelement mit der ID "name" aktualisieren, wenn die Verbindung geschlossen ist
-        document.getElementById("name").textContent = "WebSocket Verbindung geschlossen";
-        // Hier kannst du je nach Bedarf die Verbindung erneut herstellen.
+        const parts = receivedMessage.split(";");
+        
+        document.getElementById("test").textContent = parts[0];
+        document.getElementById("year").textContent = parts[1] + " " + parts[2];
+        document.getElementById("name").textContent = parts[3];
     };
 
     socket.onerror = function (event) {
         console.error("Verbindungsfehler! Wiederholung in 30 Sekunden...", event);
-        // Dokumentelement mit der ID "name" aktualisieren, wenn ein Verbindungsfehler auftritt
         document.getElementById("name").textContent = "WebSocket Fehler. Wiederholung in 30 Sekunden...";
         setTimeout(connectWebSocket, 30000);
     };
